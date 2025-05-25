24ad951f6ab1a608fbe4ec13b0227e9e161365cc
ObAkinyemi <beteiscoo@gmail.com>
2025-05-24 23:32:12 -0600

    Just got it working with an async function. async function fetchData is far more modular and adaptable than just the fetch method. Now I need to understand what all of this code is doing and how it's working together before I continue onwards. I now understand how I can use this for the world clock thingy.

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu") //promise based
-.then(response => {
-    if(!response.ok){
-        throw new Error("could not fetch resource");
-    }
-    return response.json();
-})
-.then(data => console.log(data))
-.catch(error => console.error(error)); //catches errors
+fetchPokeData();
 
-const cityBtn = document.getElementById("enter_button");
+
+const cityBtn = document.getElementById("fetch_city");
 cityBtn.addEventListener("click", getCity);
+
+const pokeBtn = document.getElementById("fetch_pokemon");
+pokeBtn.addEventListener("click", fetchPokeData);
 
 function getCity () {
     console.log(document.getElementById("city_in").value);
+}
+
+async function fetchPokeData() {
+    try{
+        const pokename = document.getElementById("pokemon_in").value.toLowerCase();
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokename}`);
+
+        if(!response.ok){
+            throw new Error("Could not fetch data");
+        }
+
+        const data = await response.json();
+        // console.log(data);
+        const pokemonSprite = data.sprites.front_default;
+        const pokeImage = document.getElementById("pokemon-sprite");
+        pokeImage.src = pokemonSprite;
+        pokeImage.style.display = "block";
+    }
+    catch(error){
+        console.error(error);
+    }
 }
