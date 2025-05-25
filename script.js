566905ac9512ac3921777c5cf53531182fe28c7b
ObAkinyemi <beteiscoo@gmail.com>
2025-05-25 16:33:16 -0600

    I'm gonna figure out how to use unix time for all the data properties I want to use.

--- script.js
+++ script.js
@@ -5,6 +5,46 @@ const cityBtn = document.getElementById("fetch_tz");
 cityBtn.addEventListener("click", getCity);
 
 
+async function getCity () {
+
+    try {
+        const timezone = document.getElementById("timezones").value;
+        const time_disp = document.getElementById("time-disp");
+        let timezone_info = timezone.split("/");
+        let slash_Count = 0;
+        let response;
+        console.log(timezone);
+        console.log(timezone_info);
+        console.log(typeof(timezone));
+        
+        for (let i in timezone_info){
+            slash_Count++;
+        }
+        slash_Count -= 1;
+        
+        if (slash_Count == 2){
+            response = await fetch(`http://worldtimeapi.org/api/timezone/${timezone_info[0]}/${timezone_info[1]}/${timezone_info[2]}`);
+        } else if(slash_Count == 1){
+            response = await fetch(`http://worldtimeapi.org/api/timezone/${timezone_info[0]}/${timezone_info[1]}`);
+        } else if(slash_Count == 0){
+            response = await fetch(`http://worldtimeapi.org/api/timezone/${timezone_info[0]}`);
+        } else{
+            console.error("The response elif block isn't working.");
+        }
+        if (!response.ok){
+            throw new Error("Could Not Fetch Data and elif block isn't working");
+        }  
+        
+        let data = await response.json();
+        console.log(data);
+
+
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
 async function fetchTimezones() {
     try {
         const response = await fetch("timezones.json");
@@ -24,44 +64,4 @@ async function fetchTimezones() {
     } catch (error) {
         console.error('Could not fetch the json file', error);
     }
-}
-
-async function getCity () {
-
-
-    try {
-        const timezone = document.getElementById("timezones").value;
-        let timezone_info = timezone.split("/");
-        let slash_Count = 0;
-        let response;
-        console.log(timezone);
-        console.log(timezone_info);
-        console.log(typeof(timezone));
-        
-        for (i in timezone_info){
-            slash_Count++;
-        }
-        slash_Count -= 1;
-        
-        if (slash_Count == 2){
-            response = await fetch(`http://worldtimeapi.org/api/timezone/${timezone_info[0]}/${timezone_info[1]}/${timezone_info[2]}`);
-        } else if(slash_Count == 1){
-            response = await fetch(`http://worldtimeapi.org/api/timezone/${timezone_info[0]}/${timezone_info[1]}`);
-        } else if(slash_Count == 0){
-            response = await fetch(`http://worldtimeapi.org/api/timezone/${timezone_info[0]}`);
-        } else{
-            console.error("The response elif block isn't working.");
-        }
-
-        if (!response.ok){
-            throw new Error("Could Not Fetch Data and elif block isn't working");
-        }  
-        
-        let data = await response.json();
-        console.log(data);
-
-    } catch (error) {
-        console.error(error);
-    }
-
 }
